9bbaf91bd38782a18ed365c6fdab6441c88ab0bb
mansiachuthan <mansi.achuthan@gmail.com>
2022-01-07 13:28:16 -0800

    added remove feature

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,31 +1,67 @@
-import React from 'react'
-import Table from './Table'
+import React, {useState} from 'react';
+import Table from './Table';
 
-const characters = [
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-];
+// const characters = [
+//     {
+//       name: 'Charlie',
+//       job: 'Janitor',
+//     },
+//     {
+//       name: 'Mac',
+//       job: 'Bouncer',
+//     },
+//     {
+//       name: 'Dee',
+//       job: 'Aspring actress',
+//     },
+//     {
+//       name: 'Dennis',
+//       job: 'Bartender',
+//     },
+// ];
+
+// function MyApp() {
+//     return (
+//         <div className="container">
+//           <Table characterData={characters} />
+//         </div>
+//   );  
+// }
+
+// export default MyApp;
 
 function MyApp() {
-    return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-  );  
+  const [characters, setCharacters] = useState([
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspiring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]);  
+
+function removeOneCharacter (index) {
+  const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated);
+  }
+
+return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    </div>
+  )
 }
 
 export default MyApp;
