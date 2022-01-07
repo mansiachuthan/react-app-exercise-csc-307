f378d44110d56c6c1f4e0eb971d6f35c0f66f7b8
mansiachuthan <mansi.achuthan@gmail.com>
2022-01-07 13:41:40 -0800

    can now add entries and remove entries

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,54 +1,9 @@
 import React, {useState} from 'react';
 import Table from './Table';
-
-// const characters = [
-//     {
-//       name: 'Charlie',
-//       job: 'Janitor',
-//     },
-//     {
-//       name: 'Mac',
-//       job: 'Bouncer',
-//     },
-//     {
-//       name: 'Dee',
-//       job: 'Aspring actress',
-//     },
-//     {
-//       name: 'Dennis',
-//       job: 'Bartender',
-//     },
-// ];
-
-// function MyApp() {
-//     return (
-//         <div className="container">
-//           <Table characterData={characters} />
-//         </div>
-//   );  
-// }
-
-// export default MyApp;
+import Form from './Form';
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspiring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]);  
+    const [characters, setCharacters] = useState([]); 
 
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
@@ -57,9 +12,13 @@ function removeOneCharacter (index) {
     setCharacters(updated);
   }
 
+function updateList(person) {
+    setCharacters([...characters, person]);
+  }
 return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
   )
 }
