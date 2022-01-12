a2f2a78969ab27015dbfdbfe7bca3f450c1dd4cf
mansiachuthan <mansi.achuthan@gmail.com>
2022-01-12 12:56:03 -0800

    linked to backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
 function MyApp() {
     const [characters, setCharacters] = useState([]); 
@@ -15,6 +16,26 @@ function removeOneCharacter (index) {
 function updateList(person) {
     setCharacters([...characters, person]);
   }
+
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:5000/users');
+       console.log(response);
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+          }
+}
+useEffect(() => {
+ console.log("in useEffect");
+    fetchAll().then( result => {
+   if (result)
+            setCharacters(result);
+          });
+}, [] );
 return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
@@ -22,5 +43,4 @@ return (
     </div>
   )
 }
-
 export default MyApp;
